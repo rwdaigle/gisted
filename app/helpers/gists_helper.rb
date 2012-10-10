e8aba4ae15532b410e870a22a14a035000c83037
Ryan Daigle <ryan.daigle@gmail.com>
2012-10-09 21:13:10 -0400

    Allow nil description gists

---
+++ app/helpers/gists_helper.rb
@@ -0,0 +1,8 @@
+module GistsHelper
+
+  def search_result_link(result)
+    text = result.highlight.description ? result.highlight['description'].first.html_safe : result.description
+    text = "<em>Untitled</em>".html_safe if text.blank?
+    link_to(text, result.url)
+  end
+end
