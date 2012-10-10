46af8592da77d7d29cba8def0664c9d09d1f058e
Ryan Daigle <ryan.daigle@gmail.com>
2012-10-09 21:38:37 -0400

    Skip home page if already logged in

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,2 +1,11 @@
 class HomesController < ApplicationController
+
+  before_filter :skip_if_logged_in, :only => :index
+
+  private
+
+  def skip_if_logged_in
+    redirect_to search_gists_path if user_logged_in?
+  end
+
 end
