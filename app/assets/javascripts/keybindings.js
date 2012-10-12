b88cc5af838027b565b37e5859afa9f06678ccc8
Ryan Daigle <ryan.daigle@gmail.com>
2012-10-12 13:59:28 -0400

    Add mouse bindings for search result navigation

---
+++ app/assets/javascripts/keybindings.js
@@ -0,0 +1,62 @@
+$(function() {
+
+  var selectedClass = 'selected';
+  var searchField = $("#search-form #q");
+
+  searchField.select();
+
+  Mousetrap.bind(['down'], function(e) {
+    navigateList('down', $("ul.search-results li:first"));
+    haltEvent(e);
+  });
+
+  Mousetrap.bind(['up'], function(e) {
+    navigateList('up', $("ul.search-results li:last"));
+    haltEvent(e);
+  });
+
+  Mousetrap.bind(['enter', 'o'], function(e) {
+    if(goToSelectedResult()) {
+      haltEvent(e);
+    }
+  });
+
+  Mousetrap.bind(['/', 's', 'escape'], function(e) {
+    searchField.focus().select();
+    $("ul.search-results li.selected").removeClass(selectedClass);
+    haltEvent(e);
+  });
+
+  var haltEvent = function(event) {
+    event.stopPropagation();
+    event.preventDefault();   
+  };
+
+  // direction == up|down
+  var navigateList = function(direction, defaultEl) {
+
+    previouslySelectedEl = $("ul.search-results li.selected");
+
+    if(previouslySelectedEl.size() <= 0) {
+      defaultEl.addClass(selectedClass);
+      searchField.blur();
+    } else {
+      previouslySelectedEl.removeClass(selectedClass);
+      newlySelectedEl = direction == 'up' ? previouslySelectedEl.prev() : previouslySelectedEl.next();
+      newlySelectedEl.addClass(selectedClass);
+      if(newlySelectedEl.size() <= 0) {
+        searchField.focus();
+      }
+    }
+  }
+
+  var goToSelectedResult = function() {
+    selectedEl = $("ul.search-results li.selected");
+    if(selectedEl.size() >= 1) {
+      location.href = selectedEl.find("a.gist-url").attr('href');
+      return true;
+    } else {
+      return false;
+    }
+  };
+});
--- app/helpers/gists_helper.rb
+++ app/helpers/gists_helper.rb
@@ -3,7 +3,7 @@ module GistsHelper
   def search_result_link(result)
     text = (result.highlight && result.highlight.description) ? result.highlight['description'].first.html_safe : result.description
     text = "<em>Untitled</em>".html_safe if text.blank?
-    link_to(text, result.url)
+    link_to(text, result.url, :class => 'gist-url')
   end
 
   def files_display(result)
