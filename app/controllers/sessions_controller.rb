fbb6e04d947f30fca514cf9b2c47d8c2b4abc05c
Ryan Daigle <ryan.daigle@gmail.com>
2012-11-24 13:01:54 -0500

    Fix user login bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     user = User.authenticate(request.env['omniauth.auth'])
     log_in_user(user.id)
     log({ns: self.class, fn: __method__, measure: true, at: 'login'}, user)
-    if(user.fetched?)
+    if(user.gists.any?)
       log({ns: self.class, fn: __method__, measure: true, at: 'repeat-login'}, user)
       redirect_to(request.env['omniauth.origin'] || search_gists_path)
     else
